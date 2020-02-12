48b8a13c437245ff05b12dc36f0cb6ccb4d59c8b
Patrick Goulding <patrickgoulding@gmail.com>
2020-02-12 10:33:07 -0700

    Initial templates structure

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
+
 def index():
-  return "Hello World"
+  user = {'username': 'Bob'}
+  posts = [
+    {
+      'author' : {'username': 'Patrick'},
+      'body' : 'Beautiful Day in Denver!'
+    },
+    {
+      'author': {'username': 'Steve'},
+      'body': 'How about them Eagles?'
+    }
+  ]
+
+  return render_template('index.html', title='home', user=user, posts=posts)
